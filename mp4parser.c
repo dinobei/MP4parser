5edb6fd2235f379fe0decc4aeaa0749247d637b4
bright.jiang <bright.jiang@infotm.com>
2016-09-22 19:27:06 +0800

    modify to old and work

--- mp4parser.c
+++ mp4parser.c
@@ -21,6 +21,9 @@ int main(int argc, char* argv[])
    fseek(file,0L,SEEK_END);
    filesize = ftell(file);
    fseek(file,0L,SEEK_SET); 
+   printf("file size ->%d\n",filesize);
+
+#if 0
    buffer = (BUFFER_t *)malloc(sizeof(BUFFER_t));
    buffer->begin_addr = (unsigned char *)malloc(filesize);
    buffer->buf = (unsigned char *)malloc(filesize);
@@ -39,19 +42,23 @@ int main(int argc, char* argv[])
    buffer_close(s);
    destory_buffer_stream(s);
    ////////////////
-
- /*  s = create_file_stream();
-   if (stream_open(s, "Channel1_Audio1_MPU_0000.mp4", MODE_READ) == 0)
+#else
+   s = create_file_stream();
+   if (stream_open(s,argv[1], MODE_READ) == 0)
       return -1;
 
    root = MP4_BoxGetRoot(s);
    SearchResult=MP4_BoxSearchBox(root,ATOM_mmth);
-   printf("search result box is %c%c%c%c\n",SearchResult->i_type&0x000000ff,(SearchResult->i_type&0x0000ff00)>>8,(SearchResult->i_type&0x00ff0000)>>16,(SearchResult->i_type&0xff000000)>>24);
+   printf("search result box is %c%c%c%c\n",
+		   SearchResult->i_type&0x000000ff,
+		   (SearchResult->i_type&0x0000ff00)>>8,
+		   (SearchResult->i_type&0x00ff0000)>>16,
+		   (SearchResult->i_type&0xff000000)>>24);
    MP4_BoxFree( root );
 
    stream_close(s);
-   destory_file_stream(s);*/
-
+   destory_file_stream(s);
+#endif
 	return 0;
 }
 
